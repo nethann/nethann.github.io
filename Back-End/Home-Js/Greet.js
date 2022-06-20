21acd9d3e3a6808f422da4ee00632feaaf0ebff8
nethann <nethan.nagendran@gmail.com>
2022-06-19 21:41:03 -0400

    Updated Website
    Updated

---
+++ Back-End/Home-Js/Greet.js
@@ -0,0 +1,19 @@
+// going to display the greeting by the time
+
+let greetingTxt = document.querySelector('.greeting');
+
+
+var CurrentDate = new Date();
+var Hours = CurrentDate.getHours();
+
+if (Hours < 12){
+    greetingTxt.innerHTML = 'Good Morning,'
+}
+
+else if (Hours >= 12 && Hours <= 17){
+    greetingTxt.innerHTML = 'Good Afternoon,'
+}
+
+else if(Hours >= 17 && Hours <= 24){
+    greetingTxt.innerHTML = 'Good Evening,'
+}
